e9339f09e789f15a98960c7b4f7978eaac306f6f
Joy <github.joysaha@gmail.com>
2024-02-12 09:39:43 +0600

    second commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const counter1Display = document.getElementById('counter1Display');
+const counter1IncrimentBtn = document.getElementById('counter1IncrimentBtn')
+const counter1DecrimentBtn = document.getElementById('counter1DecrimentBtn')
+
